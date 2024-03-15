72d91362c6df8a5d28e482db1996350ff1198569
jrb311 <159147700+jrb311@users.noreply.github.com>
2024-03-14 21:51:11 -0400

    Make grid change, create button to reset grid
    Add event listeners on mouseover to change grid square color purple
    Add parent div (button container) to the reset button
    Add parent div display as flex and centered button using justify content

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let gridNum = 256;
-let container = document.querySelector(".container");
+const container = document.querySelector(".container");
 
 for (let i = 0; i < 256; i++)
 {
@@ -7,3 +7,21 @@ for (let i = 0; i < 256; i++)
     grid.classList.add("childSquare");
     container.appendChild(grid);
 };
+
+const grid = document.querySelectorAll(".childSquare");
+
+grid.forEach(element => {
+    element.addEventListener('mouseover', (event) =>{
+        event.target.style.backgroundColor = 'purple';
+    });
+});
+
+const body = document.querySelector('body');
+const buttonContainer = document.createElement("div");
+const gridButton = document.createElement('button');
+buttonContainer.style.cssText = "display: flex; justify-content: center";
+gridButton.style.cssText = "border-radius: 25px; border-color: purple; font-size: 20px; background-color: gold; color: purple";
+gridButton.textContent = "Generate New Grid";
+buttonContainer.appendChild(gridButton);
+body.insertBefore(buttonContainer, container);
+
